40c49d2c160a86816eebd751d3cbccf8b0216241
Novoselov Andrey <90569831+Asurasuu@users.noreply.github.com>
2023-08-21 11:13:02 +0600

    Update db.php

--- include/db.php
+++ include/db.php
@@ -2,9 +2,9 @@
 
 $data = array(
 	"server" => "localhost",
-	"db" => "cu52444_ffm", // cn23945_ffm для тестового контура
-	"user" => "cu52444_ffm", // cu52444_ffm у дб такое же значения
-	"password" => "GodAsura535635"
+	"db" => "",
+	"user" => "",
+	"password" => ""
 );
 
 $connection = mysqli_connect($data['server'], $data['user'], $data['password'], $data['db']);
